b0f221f739203a08a20be2f1eceb0b60c4e888c3
Gökay Gürcan <gokaygurcan@gmail.com>
2016-10-04 00:43:44 +0300

    feature: Extend Express with auxiliary packages

--- index.js
+++ index.js
@@ -4,13 +4,24 @@
 
 // core modules
 const http = require('http');
+const path = require('path');
 
 // node_modules
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const favicon = require('serve-favicon');
 
 // variables
 const app = express();
 const port = process.env.PORT || '6598';
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // magic!
 app.all('/', (req, res, next) => {
@@ -31,8 +42,10 @@ const server = http.createServer(app);
 server.listen(port);
 
 // handle events
-server.on('error', (err) => {
-  console.log(err.code);
+server.on('error', (error) => {
+  console.error(chalk.red('Error occured: %s'), chalk.underline(error.code));
+  throw error;
 }).on('listening', (data) => {
-  console.log('Listening');
+  console.info(chalk.green('Started on port %s'), chalk.underline(server.address().port));
+  console.info(chalk.gray('ktrl/Ctrl + C to shut down. \n'));
 });
