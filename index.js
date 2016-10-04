37a8baafdf47e6d445cb446f2971ce5086542708
Gökay Gürcan <gokaygurcan@gmail.com>
2016-10-04 14:43:49 +0300

    Release/1.0.0 (#1)
    * chore: Add .editorconfig file
    
    * chore: Add .gitignore, .gitattributes, and .gitconfig files
    
    * chore: Initialize npm
    
    * feature: Create a basic server w/ Express
    
    * feature: Extend Express with auxiliary packages
    
    * feature: Add Universal Analytics package and make call to Google Analytics
    
    * fix: Sending data to Google is now asynchronous
    
    * feature: Add pixel support
    
    * feature: Send ip address and user agent
    
    * test: Add test suite and fix initial problems
    
    * feature: Add Travis CI integration and Snyk badge

---
+++ index.js
@@ -0,0 +1,80 @@
+// https://analytics.gokaygurcan.com/
+
+'use strict';
+
+// core modules
+const http = require('http');
+const path = require('path');
+
+// node_modules
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const ua = require('universal-analytics');
+
+// variables
+const app = express();
+const port = process.env.PORT || '6598';
+
+app.enable('trust proxy');
+app.set('x-powered-by', false);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// magic!
+app.get('/', (req, res) => {
+  res.redirect(302, 'https://www.gokaygurcan.com/');
+});
+
+app.get('/:tracking_id/*.(svg|png|gif)', (req, res) => {
+  let params = req.params;
+  let query = req.query;
+
+  // vars
+  let id = params.tracking_id;
+  let path = params['0'];
+  let ext = params['1'];
+  let style = query.style ? query.style : 'flat-square';
+  let file = ext === 'gif' ? 'pixel' : 'badge-' + style;
+
+  // create a visitor
+  let visitor = ua(id);
+
+  // hit!
+  visitor.pageview(
+    {
+      v: 1,
+      tid: id,
+      t: 'pageview',
+      uip: req.ip, // IPv6
+      ua: req.headers['user-agent'],
+      dp: path
+    }
+  ).send();
+
+  // render the badge
+  res.sendFile(`${__dirname}/public/${file}.${ext}`);
+});
+
+// set port
+app.set('port', port);
+
+// create server
+const server = http.createServer(app);
+
+// start listening
+server.listen(port);
+
+// handle events
+server.on('error', error => {
+  console.error(chalk.red(`Error occured: ${chalk.underline(error.code)}`));
+  throw error;
+}).on('listening', () => {
+  console.info(chalk.green(`Started on port ${chalk.underline(server.address().port)}`));
+  console.info(chalk.gray(`ktrl/Ctrl + C to shut down. \n`));
+});
